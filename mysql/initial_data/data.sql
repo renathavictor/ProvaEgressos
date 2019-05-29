098b11fba979157d1467f17f69512109ab5f1a60
renatha <renatha.nvictor@gmail.com>
2019-05-29 11:50:00 -0300

    data

--- mysql/initial_data/data.sql
+++ mysql/initial_data/data.sql
@@ -1,12 +1,12 @@
-CREATE DATABASE `egressos`;
-USE `egressos`;
+CREATE DATABASE egressos;
+USE egressos;
 
 
 CREATE TABLE users (
   id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
   name VARCHAR(50) NOT NULL, 
   email VARCHAR(50) NOT NULL, 
-  password VARCHAR(60) NOT NULL,
+  password VARCHAR(100) NOT NULL,
   course VARCHAR(50),
   linkedin VARCHAR(50),
   github VARCHAR(50)
